4c9ad8bcfd6f05e797103f28e3f340afc3e794ce
Shubhojeet Nayak <shubhojeetnayak5220@gmail.com>
2026-07-16 09:12:13 +0530

    Time: 62 ms (54.23%), Space: 44.1 MB (59.78%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,31 +1,25 @@
 class Solution {
     public int beautySum(String s) {
         int ans = 0;
-        for (int i = 0; i < s.length() - 2; i++) {
-            for (int j = i + 2; j < s.length(); j++) {
-                ans += frequency(s.substring(i, j + 1));
+
+        for(int i=0; i<s.length(); i++){
+            int[] freq = new int[26];
+            for(int j=i; j<s.length(); j++){
+                freq[s.charAt(j) - 'a']++;
+
+                int min = Integer.MAX_VALUE;
+                int max = Integer.MIN_VALUE;
+
+                for(int count : freq){
+                    if (count > 0) {
+                        max = Math.max(max, count);
+                        min = Math.min(min, count);
+                    }
+                }
+
+                ans += (max - min);
             }
         }
-
         return ans;
-    }
-
-    public int frequency(String s) {
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            freq[ch - 'a']++;
-        }
-
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        for (int i : freq) {
-            if (i > 0) {
-                min = Math.min(i, min);
-                max = Math.max(i, max);
-            }
-        }
-
-        return max - min;
     }
 }
