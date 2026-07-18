ba88f43dc31a832070cd1862315fe315452f62d8
Shubhojeet Nayak <shubhojeetnayak5220@gmail.com>
2026-07-18 23:02:13 +0530

    Time: 7 ms (12.14%), Space: 45.9 MB (72.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        String num = Integer.toString(x);
+        int i = 0;
+        int j = num.length()-1;
+
+        while(i <= j){
+            if(num.charAt(i) != num.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
