cb02362ce61ffbdea278d5b82a6f93d3c0e02d8e
Shubhojeet Nayak <shubhojeetnayak5220@gmail.com>
2026-07-28 21:29:43 +0530

    Time: 20 ms (83.71%), Space: 56.4 MB (76.82%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int count = 0;
+        int i = 0;
+        int j = people.length-1;
+
+        while(i <= j){
+            if(people[i] + people[j] <= limit){
+                count++;
+                i++;
+                j--;
+            }else{
+                j--;
+                count++;
+            }
+        }
+        return count;
+    }
+}
