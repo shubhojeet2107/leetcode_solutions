02d718ee66f1af1658fdfe2ff945ba358934dca0
Shubhojeet Nayak <shubhojeetnayak5220@gmail.com>
2026-08-01 10:21:48 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,5 +1,11 @@
 class Solution {
+    int[][] dp;
     public boolean predictTheWinner(int[] nums) {
+        dp = new int[nums.length+1][nums.length+1];
+        for(int i=0; i<dp.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+
         return recursive(nums, 0, nums.length-1) >= 0;
     }
 
@@ -8,7 +14,11 @@ class Solution {
             return nums[i];
         }
 
-        return Math.max(
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        return dp[i][j] = Math.max(
             nums[i] - recursive(nums, i+1, j),
             nums[j] - recursive(nums, i, j-1)
         );
