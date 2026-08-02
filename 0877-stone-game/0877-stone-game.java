461913549438e437b8d19af67f525a982b3f0098
Shubhojeet Nayak <shubhojeetnayak5220@gmail.com>
2026-08-02 10:25:59 +0530

    Time: 0 ms (100%), Space: 42.9 MB (88.59%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,21 +1,5 @@
 class Solution {
-    int[][] dp;
     public boolean stoneGame(int[] piles) {
-        dp = new int[piles.length+1][piles.length+1];
-        for(int i=0; i<dp.length; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return recursive(piles, 0, piles.length-1) >= 0;
-    }
-
-    public int recursive(int[] nums, int i, int j){
-        if(i == j) return nums[i];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = Math.max(
-            nums[i] - recursive(nums, i+1, j),
-            nums[j] - recursive(nums, i, j-1)
-        );
+        return true;
     }
 }
