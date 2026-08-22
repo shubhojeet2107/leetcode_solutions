c382ca5183845825c83b92158a24fa9b2d95aea4
Shubhojeet Nayak <shubhojeetnayak5220@gmail.com>
2026-08-22 23:19:31 +0530

    Time: 1 ms (27.91%), Space: 42.7 MB (51.58%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(') open.push(i);
+            else if(ch == '*') star.push(i);
+            else 
+            { //(ch == ')')
+                if(!open.isEmpty()) open.pop();
+                else if(!star.isEmpty()) star.pop();
+                else return false;
+            }
+        }
+
+        while(!open.isEmpty() && !star.isEmpty()){
+            if(open.peek() < star.peek()){
+                open.pop();
+                star.pop();
+            }
+            else return false;
+        }
+
+        return open.isEmpty();
+    }
+}
