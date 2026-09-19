a7c0bf5c3f67119bb316790bf4fcca4bd1f3db1f
Shubhojeet Nayak <shubhojeetnayak5220@gmail.com>
2026-09-19 12:45:05 +0530

    Time: 0 ms (100%), Space: 9.8 MB (7.97%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+        return recursive(n, 1, 1, true);
+    }
+
+    int recursive(int n, int head, int step, bool leftToRight){
+        if(n == 1) return head;
+
+        if(leftToRight || (n % 2) == 1){
+            head += step;
+        }
+
+        return recursive(n/2, head, step*2, !leftToRight);
+    }
+};
